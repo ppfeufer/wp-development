20b5eb20abc3eca267cd6643aff1c40b018d502b
Peter Pfeufer <info@ppfeufer.de>
2024-11-18 14:51:11 +0100

    [ADD] DB export and import to make targets

--- .make/conf.d/wp-cli.mk
+++ .make/conf.d/wp-cli.mk
@@ -2,9 +2,17 @@
 wp_cli := $(shell which wp-cli)
 wp_path = ./../WP-Sources
 
+# Check WP path
+.PHONY: check-wp-path
+check-wp-path:
+	@if [ ! -f $(wp_path)/wp-config.php ]; then \
+		echo "The WordPress installation does not exist."; \
+		exit 1; \
+	fi
+
 # Clear all transient caches from the WordPress database
 .PHONY: clear-transient
-clear-transient:
+clear-transient: check-wp-path
 	@echo "Clearing all transient caches from the WordPress database …"
 	@$(wp_cli) transient delete \
 		--all \
@@ -12,7 +20,7 @@ clear-transient:
 
 # Start the WP-CLI shell
 .PHONY: wp-shell
-wp-shell:
+wp-shell: check-wp-path
 	@echo "Starting the WP-CLI shell …"
 	@$(wp_cli) shell \
 		--path=$(wp_path)
@@ -38,11 +46,41 @@ uninstall-wp-cli:
 	@sudo rm -f /usr/local/bin/wp-cli
 	@rm -f $(wp_dev_root_dir)/wp-cli.phar
 
+# Export Database
+.PHONY: export-db
+export-db: check-wp-path
+	@echo "Exporting the database …"
+	@$(wp_cli) db export \
+		--path=$(wp_path) \
+		--add-drop-table
+
+# Import Database
+.PHONY: import-db
+import-db: check-wp-path
+	@echo "Importing the database …"
+	@if [ -n "$(Arguments)" ]; then \
+		if [[ -f "$(Arguments)"  && "$(Arguments)" == *.sql ]]; then \
+			echo "Importing the database from $(Arguments) …"; \
+			$(wp_cli) db import \
+				--path=$(wp_path) \
+				$(Arguments); \
+			exit 0; \
+		else \
+			echo "The specified database file is not valid."; \
+		fi \
+	else \
+		echo "No database file specified, trying the default database file …"; \
+		$(wp_cli) db import \
+			--path=$(wp_path); \
+	fi
+
 # Help message for the WP-CLI commands
 .PHONY: help
 help::
 	@echo "  $(TEXT_UNDERLINE)WP-CLI:$(TEXT_UNDERLINE_END)"
 	@echo "    clear-transient           Clear all transient caches from the WordPress database"
+	@echo "    export-db                 Export the database"
+	@echo "    import-db                 Import the database"
 	@echo "    install-wp-cli            Install WP-CLI"
 	@echo "    uninstall-wp-cli          Uninstall WP-CLI"
 	@echo "    update-wp-cli             Update WP-CLI"
--- Makefile
+++ Makefile
@@ -8,6 +8,9 @@
 #wp_dev_root_dir := $(shell dirname $(shell cd -P -- '$(shell dirname -- "$0")' && pwd -P))
 wp_dev_root_dir := $(abspath $(CURDIR)/..)
 
+Command := $(firstword $(MAKECMDGOALS))
+Arguments := $(wordlist 2,$(words $(MAKECMDGOALS)),$(MAKECMDGOALS))
+
 # Help message for the Makefile
 .PHONY: help
 help::
@@ -20,6 +23,10 @@ help::
 	@echo "  $(TEXT_UNDERLINE)General:$(TEXT_UNDERLINE_END)"
 	@echo "    help                      Show this help message"
 	@echo ""
+
+# Catchall for unknown commands to fail gracefully
+.DEFAULT:
+	@true
 
 # Include the configurations
 include .make/conf.d/*.mk
