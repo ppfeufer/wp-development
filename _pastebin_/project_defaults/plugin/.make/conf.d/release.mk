b021e7a6dd068b20837aba0841e510dd4a76b4b4
Peter Pfeufer <info@ppfeufer.de>
2024-12-09 05:10:08 +0100

    [CHANGE] Plugin defaults updated

--- _pastebin_/project_defaults/plugin/.make/conf.d/release.mk
+++ _pastebin_/project_defaults/plugin/.make/conf.d/release.mk
@@ -8,6 +8,7 @@ prepare-release: pot
 	sed -i "/ * @version /c\ * @version $$new_version" $(plugin_file); \
 	sed -i "/ * Version: /c\ * Version: $$new_version" $(plugin_file); \
 	echo "Updated version in $(TEXT_BOLD)$(plugin_file)$(TEXT_BOLD_END)"; \
+	sed -i "/\"Project-Id-Version: $(plugin_name) /c\\\"Project-Id-Version: $(plugin_name) $$new_version\\\n\"" $(plugin_translation_template); \
 	echo "Please update the changelog in $(TEXT_BOLD)CHANGELOG.md$(TEXT_BOLD_END)"
 
 # Create a new release archive
